667f00478c9cede0d54f89e5ed25472ffb989bd9
sunlulu.tomato <sunlulu.tomato@bytedance.com>
2024-02-22 13:46:19 +0800

    modify deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ set -e
 
 branch=$(git branch --show-current)
 commit=$(git log -1 --format=%h)
+git_remote_info=$(git remote -v | grep origin | grep fetch | awk '{print $2}')
 echo "Build info: ${branch}@${commit}"
 echo
 
@@ -17,34 +18,12 @@ npm run build
 echo 'Deploy this page to github.io'
 echo
 
-repo_name="sunlulu427.github.io"
-
-if test -d "${repo_name}"; then
-  echo Git repo $repo_name exists, delete it first
-  sudo rm -r ${repo_name}
-fi
-echo "Clone $repo_name into current dir"
-git clone git@github.com:sunlulu427/sunlulu427.github.io.git -b main
-cd ${repo_name} || exit 1
-
-echo 'Delete origin exists files'
-rm -r assets/ images/ index.html
-
-echo "Copy dist to $repo_name"
-pwd
-cp -r ../dist/* .
-# tree .
-
-echo && echo
+cd dist/
+git init
 git add .
-commit_msg="Deploy: $branch@$commit"
-git commit -a -m "$commit_msg"
-git push -u origin main
-#cp "$dist_dir"/* .
-
-# do clean job
-sudo rm -r ../dist/
-sudo rm -r ../$repo_name
+git commit -a -m "Deploy: ${branch}@${commit}"
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+git push -f "${git_remote_info}" "${current_branch}:pages"
 
 echo 'Repo：https://github.com/sunlulu427/sunlulu427.github.io'
 echo 'See: https://sunlulu427.github.io/'

29226383a5caacd6635bccc89e369fd615fb28df
sunlulu.tomato <sunlulu.tomato@bytedance.com>
2024-03-15 23:49:33 +0800

    update: update my resume

--- src/App.vue
+++ src/App.vue
@@ -14,9 +14,11 @@
         </figure>
 
         <div class="info-content">
-          <h1 class="name" title="Richard hanrick">sunlulu.tomato</h1>
-
-          <p class="title">Tiktok Live Engineer</p>
+          <h1 class="name" title="Richard hanrick">
+            孙路路<br>
+            sunlulu.tomato
+          </h1>
+          <p class="title">Tiktok-直播研发</p>
         </div>
 
         <button class="info_more-btn" data-sidebar-btn>
@@ -40,7 +42,7 @@
             </div>
 
             <div class="contact-info">
-              <p class="contact-title">Email</p>
+              <p class="contact-title">常用邮箱</p>
 
               <a href="mailto:137380027@qq.com" class="contact-link">137380027@qq.com</a>
             </div>
@@ -54,7 +56,7 @@
             </div>
 
             <div class="contact-info">
-              <p class="contact-title">Phone</p>
+              <p class="contact-title">手机号码</p>
 
               <a href="tel:+8617318596165" class="contact-link">+86 17318596165</a>
             </div>
@@ -68,9 +70,9 @@
             </div>
 
             <div class="contact-info">
-              <p class="contact-title">Birthday</p>
+              <p class="contact-title">生日</p>
 
-              <time datetime="1994-09-20">Sep 20, 1994</time>
+              <time datetime="1994-09-20">1994-09-20</time>
             </div>
 
           </li>
@@ -82,9 +84,9 @@
             </div>
 
             <div class="contact-info">
-              <p class="contact-title">Location</p>
+              <p class="contact-title">现居地</p>
 
-              <address>Shanghai, China</address>
+              <address>上海市浦东新区</address>
             </div>
 
           </li>
@@ -168,16 +170,29 @@
       <article class="about  active" data-page="about">
 
         <header>
-          <h2 class="h2 article-title">About me</h2>
+          <h2 class="h2 article-title">关于我</h2>
         </header>
 
         <section class="about-text">
           <p>
-            I'm a live streaming engineer in tiktok.
+            我现在在字节跳动的视频架构流媒体国际部门工作，是LivePusher-Android方向Owner，负责直播推流端需求研发和QoS/QoE优化等工作。
+          </p>
+          <p>
+            字节跳动是我目前唯一工作过的公司，也是最早发给我实习Offer的公司（2019年1月），虽然当时因实验室项目原因未能成行，但从那之后逐渐关注
+            字节，了解互联网公司的工作方式和文化（说实话我是斗鱼的忠实观众，当时真不知道抖音直播有这么多人看=。=），好在后面秋招中顺利加入字节直播
+            中台营收团队，开启苦逼且美妙的职业生涯。现在回想起来，和健楠结对编程的时光仍然是工作以来最美好的回忆。
           </p>
 
           <p>
-            My job is to develop new features for Tiktok Live and optimize the QoS and QoE performance.
+            2021年因为对上海的执念和合作团队的邀请，我从北京来到了上海，也从直播中台营收组来到了视频架构。开始做抖音、Tiktok还有火山等App的直播推流端
+            工作，负责的内容包括主播端的音视频采集、特效、混流、编码以及传输等。这其实也是我在直播中台工作的延续，在营收组我主要负责的是KTV相关的需求和直播
+            推流有着最为密切的关系，比如连麦能力、音乐的解码渲染、利用Sei进行歌词同步等等。
+          </p>
+
+          <p>
+            2023年，因为🇺🇸对Tiktok的求追猛打，我们部门进行了划分，国内业务和国际业务进一步切割，我被分配到国际化部门，只负责Tiktok推流相关的业务，也就是我目前做的这些工作。
+          </p>
+          <p>
           </p>
         </section>
 
@@ -188,7 +203,7 @@
 
         <section class="service">
 
-          <h3 class="h3 service-title">What i'm doing</h3>
+          <h3 class="h3 service-title">我在做啥</h3>
 
           <ul class="service-list">
 
@@ -200,29 +215,24 @@
               </div>
 
               <div class="service-content-box">
-                <h4 class="h4 service-item-title">New features development</h4>
-
+                <h4 class="h4 service-item-title">需求研发</h4>
                 <p class="service-item-text">
-                  Develop new features for Tiktok LiveStreaming
+                  主要围绕团队OKR目标、业务部分提过来的需求以及自驱的技术需求
                 </p>
               </div>
-
             </li>
 
             <li class="service-item">
-
               <div class="service-icon-box">
                 <img src="/images/icon-dev.svg" alt="Web development icon" width="40">
               </div>
 
               <div class="service-content-box">
-                <h4 class="h4 service-item-title">Qos & QoE Optimization</h4>
-
+                <h4 class="h4 service-item-title">Qos/QoE优化</h4>
                 <p class="service-item-text">
-                  Continuously optimizing QoS and QoE performance to make Tiktok the best application
+                  音视频团队的宿命。推流核心指标QoS建设、优化，同时对业务侧相关的QoE指标负责。
                 </p>
               </div>
-
             </li>
 
             <li class="service-item">
@@ -232,13 +242,11 @@
               </div>
 
               <div class="service-content-box">
-                <h4 class="h4 service-item-title">Stability</h4>
-
+                <h4 class="h4 service-item-title">稳定性相关</h4>
                 <p class="service-item-text">
-                  Improve push stream success rate and reduce crash, OOM and ANR rate.
+                  广义上的稳定性，包括推流成功率，直播可用性，以及Crash、OOM和ANR等问题。
                 </p>
               </div>
-
             </li>
 
             <li class="service-item">
@@ -248,19 +256,25 @@
               </div>
 
               <div class="service-content-box">
-                <h4 class="h4 service-item-title">Architecture</h4>
-
+                <h4 class="h4 service-item-title">工程架构</h4>
                 <p class="service-item-text">
-                  Take your code architecture seriously and take responsibility for its quality.
+                  包括代码设计架构、工程组织架构以及CI/CD等
                 </p>
               </div>
 
             </li>
 
           </ul>
-
+          <h3 class="h3 service-title" style="margin-top: 20px;">兴趣爱好</h3>
+          <p class="about-text">
+            <b>篮球、羽毛球、骑车</b>：这类运动可以随时叫上我，我是专业凑人数的。<br>
+            <b>王者农药</b>：已经被女朋友带到了星耀二；主玩射手，艾琳和虞姬，对抗路只玩芈月，中路只玩嬴政，打野只玩元芳。<br>
+            <b>云顶之弈</b>：S1最强王者，就是那个卡萨丁一刀一个的版本，现在是云玩家。<br>
+            <b>象棋</b>：天天象棋和揭棋都是业6。偶尔会关注象甲联赛。<br>
+            <b>麻将</b>：去年被同学带入门后，觉得蛮有意思，可惜啊，很难凑齐人手。<br>
+            <b>做菜</b>：家里的主厨，拿手菜牛肉香芹，里脊芦笋。
+          </p>
         </section>
-
 
         <!--
           - testimonials
@@ -470,7 +484,7 @@
       <article class="resume" data-page="resume">
 
         <header>
-          <h2 class="h2 article-title">Resume</h2>
+          <h2 class="h2 article-title">简历</h2>
         </header>
 
         <section class="timeline">
@@ -480,31 +494,23 @@
               <ion-icon name="book-outline"></ion-icon>
             </div>
 
-            <h3 class="h3">Education</h3>
+            <h3 class="h3">教育经历</h3>
           </div>
 
           <ol class="timeline-list">
-
             <li class="timeline-item">
-
-              <h4 class="h4 timeline-item-title">[Master] Hefei University of Technology</h4>
-
-              <span>2017/09 - 2020/06  Control Theory and Control Engineering</span>
-
+              <h4 class="h4 timeline-item-title">[硕士] 合肥工业大学</h4>
+              <span>2017/09-2020/06  控制理论与控制工程</span>
               <p class="timeline-text">
-                Research interests: Kalman Filter Algorithms, Fault Diagnosis and Prediction, Fault-tolerant Control
+                主要研究方向是基于卡尔曼滤波算法的故障诊断系统，常用Matlab、Python、C++等语言。<br>
+                多次获得一等奖学金，2次华为软件精英挑战赛上合赛区二等奖，1次全国大学生数学建模竞赛三等奖。
               </p>
-
             </li>
-
             <li class="timeline-item">
-
-              <h4 class="h4 timeline-item-title">[Bachelor] Hefei University of Technology</h4>
-
-              <span>2013/09 — 2017/06 Automation Engineering</span>
-
+              <h4 class="h4 timeline-item-title">[学士] 合肥工业大学</h4>
+              <span>2013/09—2017/06 自动化</span>
               <p class="timeline-text">
-                Bachelor
+                学习了C/C++，计算机组成原理，计算机网络，操作系统，汇编语言，模数电，单片机及信号处理等课程，考研专业分第一。
               </p>
             </li>
           </ol>
@@ -512,93 +518,101 @@
         </section>
 
         <section class="timeline">
-
           <div class="title-wrapper">
             <div class="icon-box">
               <ion-icon name="book-outline"></ion-icon>
             </div>
-
-            <h3 class="h3">Experience</h3>
+            <h3 class="h3">工作经历</h3>
           </div>
-
           <ol class="timeline-list">
-
             <li class="timeline-item">
-
-              <h4 class="h4 timeline-item-title">Tiktok LiveStreaming Engineering in Bytedance</h4>
-
-              <span>2020/06 — Present</span>
-
+              <h4 class="h4 timeline-item-title">字节跳动-视频架构（上海）</h4>
+              <span>2021/09—现在</span>
               <p class="timeline-text">
-                Pushing side
+                负责直播主播端音视频采集、特效、混流、编码及推流等。<br>
+                主要技术栈：<b>C/C++、OpenGL ES及FFmpeg</b>等。 <br>
+                2022年7-8月获得超出预期的成果Spot Bonus。<br>
+                2023年上半年度公司范围内最佳字节范项目奖（10个项目之一）。
               </p>
-
             </li>
-
+            <li class="timeline-item">
+              <h4 class="h4 timeline-item-title">字节跳动-直播中台（北京）</h4>
+              <span>2020/06—2021/09</span>
+              <p class="timeline-text">
+                负责营收和垂直场景相关业务，礼物、KTV方向需求研发。<br>
+                主要技术栈：<b>Android、Kotlin、RxJava、Fresco、Retrofit、Hilt及FFmpeg</b>等。<br>
+                获得2021年7-8月最佳字节范Spot Bonus。
+              </p>
+            </li>
           </ol>
-
         </section>
-
         <section class="skill">
-
-          <h3 class="h3 skills-title">My skills</h3>
-
+          <h3 class="h3 skills-title">我的技能</h3>
           <ul class="skills-list content-card">
-
             <li class="skills-item">
-
               <div class="title-wrapper">
-                <h5 class="h5">Kotlin</h5>
-                <data value="80">80%</data>
+                <h5 class="h5">音视频相关知识、直播音视频链路等</h5>
+                <data value="70">熟悉</data>
               </div>
-
-              <div class="skill-progress-bg">
-                <div class="skill-progress-fill" style="width: 80%;"></div>
-              </div>
-
-            </li>
-
-            <li class="skills-item">
-
-              <div class="title-wrapper">
-                <h5 class="h5">Java/C++/Python</h5>
-                <data value="70">70%</data>
-              </div>
-
               <div class="skill-progress-bg">
                 <div class="skill-progress-fill" style="width: 70%;"></div>
               </div>
-
             </li>
-
             <li class="skills-item">
-
               <div class="title-wrapper">
-                <h5 class="h5">FFmpeg/OpenGL</h5>
-                <data value="90">70%</data>
+                <h5 class="h5">编程语言：Kotlin/C/C++/Java</h5>
+                <data value="70">常用，日常需求开发就是这几种语言</data>
               </div>
-
               <div class="skill-progress-bg">
                 <div class="skill-progress-fill" style="width: 70%;"></div>
               </div>
-
             </li>
-
             <li class="skills-item">
-
               <div class="title-wrapper">
-                <h5 class="h5">JavaScript/HTML</h5>
-                <data value="50">40%</data>
+                <h5 class="h5">编程语言：Python/JavaScript</h5>
+                <data value="50">熟悉，写个前后端以及CI/CD脚本没有任何问题</data>
               </div>
-
+              <div class="skill-progress-bg">
+                <div class="skill-progress-fill" style="width: 50%;"></div>
+              </div>
+            </li>
+            <li class="skills-item">
+              <div class="title-wrapper">
+                <h5 class="h5">OpenGL ES/FFmpeg/MediaCodec</h5>
+                <data value="60">主流音视频处理框架或技术</data>
+              </div>
+              <div class="skill-progress-bg">
+                <div class="skill-progress-fill" style="width: 60%;"></div>
+              </div>
+            </li>
+            <li class="skills-item">
+              <div class="title-wrapper">
+                <h5 class="h5">Android开发相关框架：RxJava, Retrofit, Dagger2，Koin等</h5>
+                <data value="40">熟悉，但目前用的少了</data>
+              </div>
               <div class="skill-progress-bg">
                 <div class="skill-progress-fill" style="width: 40%;"></div>
               </div>
-
             </li>
-
+            <li class="skills-item">
+              <div class="title-wrapper">
+                <h5 class="h5">工程相关：Gradle、CMake</h5>
+                <data value="50">熟悉，LivePusher工程架构及CI/CD是我的兼职</data>
+              </div>
+              <div class="skill-progress-bg">
+                <div class="skill-progress-fill" style="width: 50%;"></div>
+              </div>
+            </li>
+            <li class="skills-item">
+              <div class="title-wrapper">
+                <h5 class="h5">前沿技术：KCP、KMM等</h5>
+                <data value="50">很感兴趣，业余时间在学习了解，比如音视频框架的KMM版本</data>
+              </div>
+              <div class="skill-progress-bg">
+                <div class="skill-progress-fill" style="width: 30%;"></div>
+              </div>
+            </li>
           </ul>
-
         </section>
 
       </article>
@@ -848,11 +862,9 @@
       <article class="blog" data-page="projects">
 
         <header>
-          <h2 class="h2 article-title">Projects</h2>
+          <h2 class="h2 article-title">一些项目</h2>
         </header>
-
         <section class="blog-posts">
-
           <ul class="blog-posts-list">
             <li class="blog-post-item">
               <a href="https://github.com/sunlulu427/AutoConverter" target="_blank">
@@ -867,7 +879,7 @@
                   </div>
                   <h3 class="h3 blog-item-title">Auto Converter</h3>
                   <p class="blog-text">
-                    generate json related functions for data class via <b>ksp</b> and <b>kotlin-poet</b>
+                    为kotlin data class自动生成json相关函数的<b>ksp/kcp</b>插件。
                   </p>
                 </div>
               </a>
@@ -885,7 +897,7 @@
                   </div>
                   <h3 class="h3 blog-item-title">Gitlab Open API Library</h3>
                   <p class="blog-text">
-                    a gitlab openapi library based on kotlin <b>coroutines</b>, <b>retrofit</b> and <b>kotlinx.serialization</b>.
+                    基于<b>coroutines</b>, <b>retrofit</b>和<b>kotlinx.serialization</b>封装的Gitlab Open API v4版本，文档由Dokka搭建。
                   </p>
                 </div>
               </a>
@@ -904,7 +916,7 @@
                   </div>
                   <h3 class="h3 blog-item-title">Python Serialization</h3>
                   <p class="blog-text">
-                    a python serialization framework based on <b>python typing annotations</b>.
+                    基于Python 3.6+的<b>typing</b>注解的序列化/反序列化框架，麻麻再也不担心我写的Python接口没类型了！
                   </p>
                 </div>
               </a>
@@ -1062,11 +1074,9 @@
       -->
 
       <article class="contact" data-page="contact">
-
         <header>
-          <h2 class="h2 article-title">Contact</h2>
+          <h2 class="h2 article-title">联系我</h2>
         </header>
-
         <section class="mapbox" data-mapbox>
           <figure>
             <iframe
@@ -1076,26 +1086,20 @@
         </section>
 
         <section class="contact-form">
-
-          <h3 class="h3 form-title">Contact Form</h3>
-
-          <form action="#" class="form" data-form>
-
-            <div class="input-wrapper">
-              <input type="text" name="fullname" class="form-input" placeholder="Full name" required data-form-input>
-
-              <input type="email" name="email" class="form-input" placeholder="Email address" required data-form-input>
+          <h3 class="h3 form-title" >居家状态</h3>
+          <div style="display: flex; justify-content: center; align-items: center;">
+            <div style="width: 50%;"> 
+              <p class="about-text">
+                看电视（斗鱼、Bilibili）、写代码 <br>
+                看书（现在更喜欢看电子版而不是纸质版）<br>
+                如果现在的新书都可以直接购买电子版就好了
+              </p>
+              <img src="/images/desktop.jpg" alt="工作台" style="width: 95%;">
             </div>
-
-            <textarea name="message" class="form-input" placeholder="Your Message" required data-form-input></textarea>
-
-            <button class="form-btn" type="submit" disabled data-form-btn>
-              <ion-icon name="paper-plane"></ion-icon>
-              <span>Send Message</span>
-            </button>
-
-          </form>
-
+            <div style="width: 50%;">
+              <img src="/images/books.jpg" alt="常看书籍" style="width: 95%;">
+            </div>
+          </div>
         </section>
 
       </article>
